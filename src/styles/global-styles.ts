3a8618f64aaf293c3bcfe8c600524e2a48a4d485
Daniel Sobrinho <dev.sobrinho@gmail.com>
2022-05-04 11:01:16 -0300

    :lipstick: global styles

---
+++ src/styles/global-styles.ts
@@ -0,0 +1,51 @@
+import { createGlobalStyle } from 'styled-components';
+import { resetCSS } from './reset-css';
+
+export const GlobalStyles = createGlobalStyle`
+
+
+  ${resetCSS}
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+    scroll-behavior: smooth;
+  }
+
+  body {
+    font-size: 1.6rem;
+    font-family: ${({ theme }) => theme.colors.white};
+    position: relative;
+    overflow-x: hidden;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: ${({ theme }) => theme.font.family.default};
+    font-weight: 400;
+    /* margin: ${({ theme }) => theme.spacings.large} 0; */
+  }
+
+  p {
+    /* margin: ${({ theme }) => theme.spacings.medium} 0; */
+  }
+
+  ul, ol {
+    /* margin: ${({ theme }) => theme.spacings.medium}; */
+    /* padding: ${({ theme }) => theme.spacings.medium}; */
+  }
+
+  a {
+    color: ${({ theme }) => theme.colors.primaryBlue};
+    text-decoration: none;
+  }
+
+  .table {
+    width: 100%;
+    overflow-y: auto;
+  }
+`;
---
+++ src/styles/theme.ts
@@ -0,0 +1,103 @@
+export const theme: Theme = {
+  colors: {
+    primaryBlue: '#40BFFF',
+    neutralLight: '#EBF0FF',
+    neutralDark: '#223263',
+    primaryRed: '#FB7181',
+    primaryYellow: '#FFC833;',
+    primarySoftGrey: '#D1D6E8',
+    primaryPurple: '#5C61F4',
+    primaryGreen: '#53D1B6',
+    gray: '#999999',
+    grayLight: '#DFDEDE',
+    blackText: '#393939',
+    white: '#FFFFFF',
+    // grayDark: "string;",
+    // secondary: "string;",
+  },
+  font: {
+    family: {
+      default: "'Open Sans', sans-serif",
+      primary: "'Poppins', sans-serif",
+      secondary: "'Montserrat', sans-serif",
+      alternative: "'Raleway', sans-serif",
+    },
+    sizes: {
+      xsmall: '8rem',
+      small: '1.6rem',
+      medium: '2.4rem',
+      large: '3.2rem',
+      xlarge: '4.0rem',
+      xxlarge: '4.8rem',
+      huge: '5.6rem',
+      xhuge: '6.4rem',
+    },
+  },
+  media: {
+    lteMedium: '(max-width: 768px)',
+  },
+  spacings: {
+    xsmall: '8rem',
+    small: '1.6rem',
+    medium: '2.4rem',
+    large: '3.2rem',
+    xlarge: '4.0rem',
+    xxlarge: '4.8rem',
+    huge: '5.6rem',
+    xhuge: '6.4rem',
+  },
+};
+
+export type Theme = {
+  colors: {
+    neutralLight: string;
+    neutralDark: string;
+    primaryBlue: string;
+    primaryRed: string;
+    primaryYellow: string;
+    primarySoftGrey: string;
+    primaryPurple: string;
+    primaryGreen: string;
+    gray: string;
+    grayLight: string;
+    // grayDark: string;
+    // secondary: string;
+    white: string;
+    blackText: string;
+
+    // black: string;
+    // gray700: string;
+    // gray500: string;
+  };
+  font: {
+    family: {
+      default: string;
+      primary: string;
+      secondary: string;
+      alternative: string;
+    };
+    sizes: {
+      xsmall: string;
+      small: string;
+      medium: string;
+      large: string;
+      xlarge: string;
+      xxlarge: string;
+      huge: string;
+      xhuge: string;
+    };
+  };
+  media: {
+    lteMedium: string;
+  };
+  spacings: {
+    xsmall: string;
+    small: string;
+    medium: string;
+    large: string;
+    xlarge: string;
+    xxlarge: string;
+    huge: string;
+    xhuge: string;
+  };
+};
