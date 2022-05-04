bc1d8432d54b635c3309fec9ddb10f0fb5d9553d
Daniel Sobrinho <dev.sobrinho@gmail.com>
2022-05-04 11:47:12 -0300

    :bricks: create page home

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next';
+
+import HomeTemplates from '../templates/Home';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <HomeTemplates />
+    </>
+  );
+};
+
+export default Home;
+
+//
